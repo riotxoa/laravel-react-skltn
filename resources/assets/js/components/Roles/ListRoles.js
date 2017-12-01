cbae67604920adc0afbdc4f5c5bbb36018ff6435
riotxoa <riotxoa@gmail.com>
2017-12-01 09:35:40 +0100

    Roles CRUD component with filter and pagination

--- resources/assets/js/components/Roles/ListRoles.js
+++ resources/assets/js/components/Roles/ListRoles.js
@@ -1,18 +1,5 @@
 import React, { Component } from 'react';
-import { NavLink } from 'react-router-dom';
-import axios from 'axios';
-
-
-import AppBar from 'material-ui/AppBar';
-import FlatButton from 'material-ui/FlatButton';
-import RaisedButton from 'material-ui/RaisedButton';
-import { Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn } from 'material-ui/Table';
-import Dialog from 'material-ui/Dialog';
-import FloatingActionButton from 'material-ui/FloatingActionButton';
-import ContentAdd from 'material-ui/svg-icons/content/add';
-import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar';
-import Snackbar from 'material-ui/Snackbar';
-import FontIcon from 'material-ui/FontIcon';
+import DataTabla from '../Interface/DataTabla';
 
 class ListRoles extends Component {
 
@@ -20,185 +7,50 @@ class ListRoles extends Component {
         super(props);
 
         this.state = {
-            value: '',
-            smShow: false,
-            snack: {
-                open: (this.props.location.snack ? this.props.location.snack.open : false),
-                message: (this.props.location.snack ? this.props.location.snack.message : ''),
-            },
-            selected: [],
-            roles: [],
+            data: [],
         };
-
-        this.handleRowSelection = this.handleRowSelection.bind(this);
-        this.handleDeleteRole = this.handleDeleteRole.bind(this);
-        this.handleRequestSnackClose = this.handleRequestSnackClose.bind(this);
     }
 
     componentDidMount(){
         axios.get('/roles')
         .then(response => {
-            this.setState({ roles: response.data });
+            this.setState({
+                data: response.data,
+            });
         })
         .catch(function (error) {
             (error);
         })
     }
 
-    isSelected(index) {
-        return this.state.selected.indexOf(index) !== -1;
-    }
-
-    handleRowSelection(selectedRows) {
-        this.setState({
-            selected: selectedRows,
-        });
-    }
-
-    handleDeleteRole(){
-        let index = this.state.selected;
-        let id = this.state.roles[index].id;
-
-        this.setState({
-            delete_id: id,
-            delete_index: index,
-            smShow: true
-        });
-    }
-
-    handleRequestSnackClose() {
-        this.setState({
-            snack: {
-                open: false,
-                message: '',
-            }
-        });
-    }
-
     render() {
-        const styles = {
-            dialog: {
-                marginBottom:12,
-                marginLeft:12,
-                marginTop:12
-            },
-            floating: {
-                bottom:75,
-                float:'right',
-                marginTop:20,
-                position:'fixed',
-                right:75,
-            },
-            toolbar: {
-                backgroundColor:'#FFFFFF',
-            },
-            flatNew: {
-                float: 'right',
-            }
-        }
 
-        /* Configuración de la ventana modal para confirmar borrado */
-        let dcmClose = () => this.setState({ smShow: false });
-        let dcmDelete = () => {
-            this.setState({ smShow: false });
-
-            let uri = `/roles/` + this.state.delete_id;
-
-            axios.delete(uri).then((response) => {
-                let rolesArr = this.state.roles;
-                rolesArr.splice(this.state.delete_index, 1);
-                this.setState({
-                    snack: {
-                        message: 'Perfil borrado',
-                        open: true
-                    },
-                    selected: [],
-                    roles: rolesArr
-                });
-            });
-        }
-        const actions = [
-            <RaisedButton label="Cancelar" style={styles.dialog} keyboardFocused={true} onClick={dcmClose} />,
-            <RaisedButton label="Borrar" primary={true} style={styles.dialog} onClick={dcmDelete} />,
+        const TABLE_COLUMNS = [
+          {
+            key: 'name',
+            label: 'Nombre del Perfil',
+            sortable: true,
+          }, {
+            key: 'description',
+            label: 'Descripción',
+            sortable: true,
+          },
         ];
-        /* /Configuración de la ventana modal para confirmar borrado */
 
-        let roles = this.state.roles.map((val, key) => {
-            return (
-                <TableRow key={key} selected={this.isSelected(key)}>
-                    <TableRowColumn>{val.name.toUpperCase()}</TableRowColumn>
-                    <TableRowColumn>{val.description}</TableRowColumn>
-                </TableRow>
-            );
-        });
-
-        let toolbar = (this.state.selected.length ?
-            [
-                <Toolbar key={1} style={styles.toolbar}>
-                    <ToolbarGroup key={11} firstChild={true}>
-                    </ToolbarGroup>
-                    <ToolbarGroup key={12}>
-                        <NavLink to={"/perfiles/editar/"+this.state.roles[this.state.selected].id}><FlatButton label="Editar" title="Editar perfil" primary={true} style={styles.flatNew} icon={<FontIcon className="fa fa-pencil" style={{fontSize:18}} />}/></NavLink>
-                        <FlatButton label="Borrar" title="Borrar perfil" primary={true} style={styles.flatNew}  onClick={this.handleDeleteRole} icon={<FontIcon className="fa fa-trash" style={{fontSize:18}} />} />
-                    </ToolbarGroup>
-                </Toolbar>
-            ]
-            :
-            [
-                <Toolbar key={2} style={styles.toolbar}>
-                    <ToolbarGroup key={21} firstChild={true}>
-                    </ToolbarGroup>
-                    <ToolbarGroup key={22}>
-                        <NavLink to="/perfiles/nuevo"><FlatButton label="Nuevo" title="Nuevo perfil" primary={true} style={styles.flatNew} icon={<FontIcon className="fa fa-plus-circle" style={{fontSize:18}} />}  /></NavLink>
-                    </ToolbarGroup>
-                </Toolbar>
-            ]
-        );
+        if (this.state.data.length == 0) return(<div></div>);
 
         return(
-            <div>
-                <AppBar
-                    title="Perfiles"
-                    iconElementLeft={<div></div>}
-                    className="app-bar no-element-left"
-                />
-
-                <div className="container">
-                    <Table onRowSelection={this.handleRowSelection}>
-                        <TableHeader>
-                            <TableRow>
-                                <TableHeaderColumn colSpan="2" style={{padding:0}}>
-                                    {toolbar}
-                                </TableHeaderColumn>
-                            </TableRow>
-                            <TableRow>
-                                <TableHeaderColumn>Nombre del Perfil</TableHeaderColumn>
-                                <TableHeaderColumn>Descripción</TableHeaderColumn>
-                            </TableRow>
-                        </TableHeader>
-                        <TableBody deselectOnClickaway={false}>
-                            {roles}
-                        </TableBody>
-                    </Table>
-
-                    <NavLink to="/perfiles/nuevo">
-                        <FloatingActionButton title="Añadir perfil" style={styles.floating}>
-                            <ContentAdd />
-                        </FloatingActionButton>
-                    </NavLink>
-                </div>
-
-                <Snackbar
-                    open={this.state.snack.open}
-                    message={this.state.snack.message}
-                    autoHideDuration={2000}
-                    onRequestClose={this.handleRequestSnackClose}
-                />
-
-                <Dialog actions={actions} modal={false} open={this.state.smShow} onRequestClose={dcmClose}>
-                    <p>¿Está seguro de que desea <b>BORRAR</b> el registro?</p>
-                </Dialog>
-            </div>
+            <DataTabla
+                singular={"perfil"}
+                plural={"perfiles"}
+                route={"perfiles"}
+                api={"roles"}
+                columns = {TABLE_COLUMNS}
+                data={this.state.data}
+                searchText={"Buscar"}
+                multiSelectable={false}
+                importCSV={false}
+            />
         );
     }
 }
